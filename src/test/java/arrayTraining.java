c9f000db989fcb9f6699661afaddb384c40f8d80
SafaTaleeb05 <safataleeb@gmail.com>
2020-12-20 07:12:58 +0200

    solving hashmap task

--- src/test/java/arrayTraining.java
+++ src/test/java/arrayTraining.java
@@ -3,6 +3,7 @@ import core.OutputConstants;
 import org.testng.annotations.Test;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 
 import static core.files.reader.ReadExcelFile.*;
 import static core.files.writer.WriteExcelFile.writeHeaderToExcelSheet;
@@ -11,13 +12,33 @@ import static core.files.writer.WriteExcelFile.writeRowToExcelSheet;
 public class arrayTraining {
     String inputExcel = InputConstants.ARRAY_TRAINING_INPUT;
     String outputFolder = OutputConstants.TRAINING_OUTPUT;
-    String latestSubTopic="";
+    String outputExcel = outputFolder + "testing2" + ".xlsx";
+    HashMap<String, ArrayList<ArrayList>> contents = new HashMap();
 
     @Test
     public void runTesting() {
         processExcel(inputExcel);
+        writeResults(contents);
 
     }
+    private void writeResults(HashMap<String, ArrayList<ArrayList>> contents) {
+        for (HashMap.Entry<String, ArrayList<ArrayList>> entry : contents.entrySet()) {
+            ArrayList<String> row = new ArrayList<>();
+            row.add(entry.getKey());
+            writeRowToExcelSheet(outputExcel, "Sheet", row);
+
+            for (ArrayList x : entry.getValue()) {
+                ArrayList<String> items = new ArrayList<>();
+                items.add("");
+                for (int counter = 0; counter < x.size(); counter++) {
+                    items.add(x.get(counter).toString());
+                    System.out.println(x.get(counter).toString());
+                }
+                writeRowToExcelSheet(outputExcel, "Sheet", items);
+            }
+        }
+    }
+
 
     private void processExcel(String inputExcel) {
         int sheetsNo = getNumberOfSheets(inputExcel);
@@ -25,18 +46,19 @@ public class arrayTraining {
             String sheetName = getSheetName(inputExcel, counter);
             processingSheet(sheetName);
         }
+
     }
 
     private void processingSheet(String sheetName) {
         ArrayList<ArrayList<String>> rows = getSheetData(inputExcel, sheetName);
         rows.remove(0);
-        String outputExcel = outputFolder + "testing"+".xlsx";
+
         writeHeaderRow(outputExcel, "Sheet");
         for (ArrayList<String> row : rows) {
-            ArrayList<String> result = processingRow(row);
-            writeRowToExcelSheet(outputExcel, "Sheet", result);
+            processingRow(row);
         }
     }
+
     private void writeHeaderRow(String outputExcel, String sheetName) {
         ArrayList<String> headers = new ArrayList<>();
         headers.add("SubTopic");
@@ -45,23 +67,25 @@ public class arrayTraining {
         writeHeaderToExcelSheet(outputExcel, sheetName, headers);
     }
 
-    private ArrayList<String> processingRow(ArrayList<String> row) {
-        ArrayList<String> formattedRow= new ArrayList<>();
-        String paragraph= row.size() > 0 ?row.get(0):"";
-        String subTopic= row.size() > 2 ?row.get(2):"";
-        String type=row.size() > 3 ?row.get(3):"";
-        System.out.println("Working on"+subTopic+"in"+type);
-        if (!latestSubTopic.equals(subTopic)){
-            formattedRow.add(subTopic);
-            latestSubTopic=subTopic;
-        }else
-            formattedRow.add("");
-
-        formattedRow.add(paragraph);
-        formattedRow.add(type);
-
-        return formattedRow;
+    private void processingRow(ArrayList<String> row) {
+        String paragraph = row.size() > 0 ? row.get(0) : "";
+        String subTopic = row.size() > 2 ? row.get(2) : "";
+        String type = row.size() > 3 ? row.get(3) : "";
+        boolean isExist = checkIfExist(subTopic);
+        ArrayList<ArrayList> all = new ArrayList<>();
+        ArrayList<String> pair = new ArrayList<>();
+        pair.add(paragraph);
+        pair.add(type);
+        all.add(pair);
+        if (isExist) {
+            contents.get(subTopic).add(pair);
+        } else
+            contents.put(subTopic, all);
     }
 
-
+    private boolean checkIfExist(String subTopic) {
+        if (contents.keySet().contains(subTopic)) {
+            return true;
+        } else return false;
+    }
 }
